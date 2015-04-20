11aff4a65acdee1dce2c0427d800766c001f7813
XWolf Override <xwolfoverride@gmail.com>
2015-04-20 18:49:36 +0200

    Centered

--- src/main.c
+++ src/main.c
@@ -20,13 +20,14 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 static void main_window_load(Window *window) {
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 120, 144, 30));
+  //s_time_layer = text_layer_create(GRect(0, 120, 144, 30));
+  s_time_layer = text_layer_create(GRect(0, 69, 144, 30));
   text_layer_set_background_color(s_time_layer, GColorBlack);
   text_layer_set_text_color(s_time_layer, GColorClear);
   
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
